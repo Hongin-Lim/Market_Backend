d7068e938208b279e2dddb4138a0bc6d725fe645
Hongin Lim <96184680+Hongin-Lim@users.noreply.github.com>
2022-05-28 12:28:44 +0900

    고객센터 전면 재구성
    고객센터 전면 재구성

--- board/admin.py
+++ board/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 from .models import Notice
-
+from .models import N_comment
+from. models import Answer
 admin.site.register(Question)
 admin.site.register(Notice)
+admin.site.register(N_comment)
+admin.site.register(Answer)
