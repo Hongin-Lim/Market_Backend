6d48cef2f4bd229556885ef166028762268e470f
Hongin Lim <96184680+Hongin-Lim@users.noreply.github.com>
2022-06-05 20:53:16 +0900

    kafka topic, dockerfile cmd 수정

--- log_to_kafka.py
+++ log_to_kafka.py
@@ -1,24 +1,22 @@
 from kafka import KafkaProducer
 from json import dumps
 
-# 카프카로 보내는 코드
+
 producer = KafkaProducer(
     acks=0,
     compression_type='gzip',
     bootstrap_servers=['172.30.1.222:31256'],
     value_serializer=lambda x: dumps(x).encode('utf-8')
 )
-# django pod안에 log 파일을 불러오고 마지막줄 한줄을 origin 이라는 변수에 담는다.
+
 inout_f = open('/apps/logs/log', 'r', encoding="UTF-8")
 inout_lines = inout_f.readlines()
 origin = inout_lines[-1]
 
 while True:
-    # django pod안에 log 파일을 마지막줄 한줄을 last 라는 변수에 담는다.
     inout_f = open('/apps/logs/log', 'r', encoding="UTF-8")
     inout_lines = inout_f.readlines()
     last = inout_lines[-1]
-    # origin과 last log 파일이 다르면 추가 된 제일 마지막줄을 전처리 하고 카프카로 보낸다.
     if last != origin:
         data = last.strip().split(' ')
         print(data)
@@ -35,7 +33,6 @@ while True:
             'item': item,
             'item_ea': item_ea,
         }
-        producer.send('logdata_test2', value=data)
+        producer.send('final_logdata', value=data)
         producer.flush()
-        origin = last # 오리진을 라스트와 똑같이 만들어 준다.
-                      # 이유는 origin이 변화없이 그대로 있으면 데이터가 들어오지 않아도 계속 다르기떄문에 무한 반복 if절이 계속 돌아가기 때문.
+        origin = last
